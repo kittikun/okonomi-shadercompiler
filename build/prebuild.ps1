a5b6d462606dc172a202f1c241c9a184bfdc5a55
kittikun <me@kittikun.jp>
2015-11-20 13:30:56 +0900

    2

--- build/prebuild.ps1
+++ build/prebuild.ps1
@@ -11,6 +11,6 @@ $buildCmd = "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
 $buildArgs = @(
 	  "$root\src\external\googletest\googletest\msvc\gtest-md.sln",
 	  "/l:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll",
-	  "/p:Configuration=$Env:CONFIGURATION",
+	  "/p:Configuration=$Env:CONFIGURATION")
 	  #"/p:Platform=$env:PLATFORM",
-	  "/verbosity:minimal")
+	  #"/verbosity:minimal")
